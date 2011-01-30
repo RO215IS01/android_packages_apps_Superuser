a56870867ee6e6bf7b21a7a5b8193d183c5fa4ed
RO215IS01 <ro215is01@gmail.com>
2011-01-30 23:13:43 +0900

    for SHARP IS01

--- Android.mk
+++ Android.mk
@@ -4,7 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := Superuser
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 
-LOCAL_CERTIFICATE := superuser
+#LOCAL_CERTIFICATE := superuser
+LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
 
